d42428c090792c17ecc96e3afbdd4ef49dca6e82
Alexandre Pereira <xanbubu@gmail.com>
2023-06-08 20:54:47 -0300

    Criando arquivo da Challenge 1

---
+++ script.js
@@ -0,0 +1,61 @@
+
+
+const textoArea = document.querySelector(".input-texto");
+const mensagem = document.querySelector(".mensagem");
+
+//	A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+function btnEncriptar() {
+
+	const textoEncripitado = encriptar(textoArea.value);
+	mensagem.value = textoEncripitado;
+	textoArea.value = "";
+
+
+
+}
+
+function encriptar(stringEncriptada) {
+
+	let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+	stringEncriptada = stringEncriptada.toLowerCase();
+
+	for (let i = 0; i < matrizCodigo.length; i++) {
+
+		if (stringEncriptada.includes(matrizCodigo[i][0])) {
+			stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+		}
+	}
+	return stringEncriptada;
+}
+
+function btnDesencriptar() {
+
+	const textoDesencripitado = desencriptar(textoArea.value);
+	mensagem.value = textoDesencripitado;
+	textoArea.value = "";
+
+
+
+}
+
+function desencriptar(stringDesencriptada) {
+
+	let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+	stringDesencriptada = stringDesencriptada.toLowerCase();
+
+	for (let i = 0; i < matrizCodigo.length; i++) {
+
+		if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+			stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+		}
+	}
+	return stringDesencriptada;
+}
+
+
+
+
